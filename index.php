5c2c535da74cf59ded6270e5055278ee9686b07b
Trevor Glassey <tkglassey@gmail.com>
2020-11-23 17:30:14 -0800

    Stuff
    Trying to add smiley

--- index.php
+++ index.php
@@ -46,4 +46,16 @@ if (file_exists($target_file)) {
   $uploadOk = 0;
 }
 
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+
 $controller->run();
